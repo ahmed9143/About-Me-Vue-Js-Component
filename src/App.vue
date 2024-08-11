0ea4a99d84cce2591da2f41cbfe63513e973a563
ahmed elsayed <ahmedelsayed9909@gmail.com>
2024-08-11 16:22:11 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+    <AboutMe />
+
+  </div>
+</template>
+
+<script>
+import AboutMe from './components/AboutMe.vue';
+
+
+
+  export default {
+    components: {
+      AboutMe
+    }
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
